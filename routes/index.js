8af9e29ffcf49997e9bc34cdcc2f8dc588970ecd
Yaroslav Molochaev <killa66@MacBook-Pro-Yaroslav.local>
2023-06-29 17:49:13 +0300

    hotfix: const res status

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const http = require('http');
 const cardRoutes = require('./cards');
 const userRoutes = require('./users');
 
@@ -6,7 +7,7 @@ router.use('/cards', cardRoutes);
 router.use('/users', userRoutes);
 
 router.use('*', (req, res) => {
-  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+  res.status(http.STATUS_CODES.NOT_FOUND).send({ message: 'Запрашиваемый ресурс не найден' });
 });
 
 module.exports = router;
