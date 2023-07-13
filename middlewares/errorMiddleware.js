e1cb3a5619e2aea45e2a253647839c91339beb91
Yaroslav Molochaev <killa66@MacBook-Pro-Yaroslav.local>
2023-07-13 12:15:14 +0300

    fixes

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,22 +1,17 @@
-module.exports.errorMiddleware = (err, req, res, next) => {
+module.exports.errorMiddleware = (err, req, res) => {
   let status = err.statusCode || 500;
   let message = err.message || 'Произошла ошибка на сервере';
 
   if (err.name === 'MongoError' && err.code === 11000) {
     status = 409;
     message = 'Пользователь с таким email уже зарегистрирован';
-  }
-
-  if (err.name === 'ValidationError') {
+  } else if (err.name === 'ValidationError') {
     status = 400;
     message = 'Переданы некорректные данные';
-  }
-
-  if (err.name === 'JsonWebTokenError') {
+  } else if (err.name === 'JsonWebTokenError') {
     status = 401;
     message = 'Некорректный токен авторизации';
   }
 
   res.status(status).send({ message });
-  next();
 };
